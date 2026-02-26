cb7b4d513c2e645bf3eafedc3fcb1fb818628938
rdandrow <rcdandrow2@gmail.com>
2026-02-26 16:18:17 -0500

    tests: add initial login page, spec, and constants files

---
+++ cypress/constants/Login.const.ts
@@ -0,0 +1,9 @@
+// Form fields
+export const USERNAME_INPUT_PLACEHOLDER_TEXT = 'Username';
+export const PASSWORD_INPUT_PLACEHOLDER_TEXT = 'Password';
+export const LOGIN_BUTTON_TITLE = 'Login';
+
+// Error handling
+export const USERNAME_REQUIRED_FORM_ERROR = `Epic sadface: Username is required`;
+export const PASSWORD_REQUIRED_FORM_ERROR = `Epic sadface: Password is required`;
+export const INVALID_CREDENTIALS_FORM_ERROR = `Epic sadface: Username and password do not match any user in this service`;
